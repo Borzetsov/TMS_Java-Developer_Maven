87fb6aec4fea7dd545b2b51ca443f3881c76deb6
Alex <borzetsov@gmail.com>
2026-05-22 14:06:09 +0700

    HomeWork23 Initial commit

---
+++ src/main/java/HomeWork23/HomeWork23.java
@@ -0,0 +1,16 @@
+/**
+ * Classname    HomeWork23
+ * @version     0.01
+ * @author      Aleksei Borzetsov
+ * date         22.05.2026
+ */
+
+package HomeWork23;
+
+public class HomeWork23 {
+
+    public void run() {
+        System.out.println("; Алексей Борзецов | Домашняя работа №23 | Выдано 20.05.2026 ;");
+        System.out.println();
+    }
+}
