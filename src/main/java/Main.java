521f2c4f3fae44d0ad7aacdd92fb1503db566443
Alex <borzetsov@gmail.com>
2026-05-22 13:58:38 +0700

    Base commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Classname    Main
+ * @version     0.01
+ * @author      Aleksei Borzetsov
+ * date         22.05.2026
+ */
+
+public class Main {
+
+    public static void main (String[] args) {
+
+    }
+}
